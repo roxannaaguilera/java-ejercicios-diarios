92bcd531e27dfe3672f4f53cc86226ba5f6c23ce
roxanna <roxannaaguilera.mad@gmail.com>
2025-09-19 12:29:00 +0200

    Ejercicios diarios java/variables

---
+++ 2025-09-19/Ejercicio01.java
@@ -0,0 +1,21 @@
+package variables_03;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sn= new Scanner(System.in);
+		
+		int base=0;
+		int altura=0;
+		
+		System.out.println("Introduce base: ");
+		base=sn.nextInt();
+		System.out.println("Introduce altura: ");
+		base=sn.nextInt();
+		
+
+	}
+
+}
